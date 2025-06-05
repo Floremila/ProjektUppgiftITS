eed7063c909215fee6af42b5690920734311ceb1
Floremila <flormequi@gmail.com>
2025-06-05 10:32:46 +0200

    first commit

---
+++ src/main/java/se/amandaflorencia/projektuppgiftits/ProjektUppgiftItsApplication.java
@@ -0,0 +1,13 @@
+package se.amandaflorencia.projektuppgiftits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektUppgiftItsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektUppgiftItsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/amandaflorencia/projektuppgiftits/ProjektUppgiftItsApplicationTests.java
@@ -0,0 +1,13 @@
+package se.amandaflorencia.projektuppgiftits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektUppgiftItsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
