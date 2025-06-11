2d8361d184ffacde32a2f8c5d7ef3f1ce6b65807
amandalyckenius <alyckenius@gmail.com>
2025-06-11 10:48:03 +0200

    Refactor: tests in authController from mocking environment to in-memory database

--- src/test/java/se/amandaflorencia/projektuppgiftits/controller/AuthControllerTest.java
+++ src/test/java/se/amandaflorencia/projektuppgiftits/controller/AuthControllerTest.java
@@ -1,30 +1,30 @@
 package se.amandaflorencia.projektuppgiftits.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import se.amandaflorencia.projektuppgiftits.dto.UserRegistrationDTO;
-import se.amandaflorencia.projektuppgiftits.service.TokenService;
-import se.amandaflorencia.projektuppgiftits.service.UserService;
+import se.amandaflorencia.projektuppgiftits.model.AppUser;
+import se.amandaflorencia.projektuppgiftits.repository.UserRepository;
 import org.springframework.http.MediaType;
 
+import static org.hamcrest.Matchers.emptyString;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @ActiveProfiles("test")
@@ -35,83 +35,84 @@ class AuthControllerTest {
     @Autowired
     private MockMvc mockMvc;
     @Autowired
-    private UserService userService;
+    private UserRepository userRepository;
     @Autowired
-    private TokenService tokenService;
-    @Autowired
-    private AuthenticationManager authenticationManager;
+    private PasswordEncoder passwordEncoder;
+    ObjectMapper mapper = new ObjectMapper(); //konverterar javaobj till json
 
-    @TestConfiguration
-    static class TestMockConfig {
-        @Bean
-        public UserService userService() {
-            return mock(UserService.class);
-        }
+    @BeforeEach
+    void setUp() {
+        userRepository.deleteAll();
 
-        @Bean
-        public TokenService tokenService() {
-            return mock(TokenService.class);
-        }
+        AppUser admin = new AppUser();
+        admin.setUsername("admin");
+        admin.setPassword(passwordEncoder.encode("Password123!!"));
+        admin.setRole("ADMIN");
+        admin.setConsentGiven(true);
 
-        @Bean
-        @Primary
-        public AuthenticationManager mockAuthenticationManager() {
-            return mock(AuthenticationManager.class);
-        }
+        userRepository.save(admin);
+
+        AppUser user = new AppUser();
+        user.setUsername("user");
+        user.setPassword(passwordEncoder.encode("Password123!!"));
+        user.setRole("USER");
+        user.setConsentGiven(true);
+
+        userRepository.save(user);
     }
 
+    private String getToken(String username, String password) throws Exception{
+        MvcResult result = mockMvc.perform(post("/users/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"username\":\""+username+"\",\"password\":\""+password+"\"}"))
+                .andExpect(status().isOk())
+                .andReturn();
 
+        return result.getResponse().getContentAsString();
+    }
 
     @Test
     void givenValidCredentials_whenLogin_thenReturnStatusOk() throws Exception {
-        Authentication mockAuth = mock(Authentication.class);
-        when(authenticationManager.authenticate(any())).thenReturn(mockAuth);       //låtsas att autentisering lyckas och returnerar mockat objekt
-
-        when(tokenService.generateToken(mockAuth)).thenReturn("fake token");       //ger tillbaka ett fake-token när controllern försöker skapa Jwt-token
-
         mockMvc.perform(post("/users/login")
-                        .content("{ \"username\": \"Amanda\", \"password\": \"Password123!\" }")
+                        .content("{ \"username\": \"user\", \"password\": \"Password123!!\" }")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string("fake token"));
+                .andExpect(content().string(Matchers.not(emptyString())));
     }
 
     @Test
     void givenInvalidCredentials_whenLogin_thenReturnUnauthorized() throws Exception{
-
-        when(authenticationManager.authenticate(any())).thenThrow(new BadCredentialsException("Invalid credentials"));  //simulerar misslyckad autentisering
-
         mockMvc.perform(post("/users/login")
-                .content("{\"username\": \"Amanda\", \"password\": \"Fel\" }")
+                .content("{\"username\": \"user\", \"password\": \"Fel\" }")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isUnauthorized());
-
     }
 
 
     @Test
-    void givenValidCredentials_whenRegister_thenReturnStatusOk() throws Exception {
+    void givenAdminToken_whenRegister_thenReturnStatusOk() throws Exception {
+        String adminToken = getToken("admin", "Password123!!");
         UserRegistrationDTO dto = new UserRegistrationDTO(
-                "Amanda",
+                "NewUser",
                 "Password123!!",
                 "USER",
                 true
         );
 
-        String jsonBody = new ObjectMapper().writeValueAsString(dto);
+        String jsonBody = mapper.writeValueAsString(dto);
 
         mockMvc.perform(post("/users/register")
+                        .header("Authorization", "Bearer " + adminToken)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(jsonBody))
                 .andExpect(status().isOk())
                 .andExpect(content().string("User successfully created"));
-
-
     }
 
 
     @Test
-    void givenInvalidCredentials_whenRegister_thenReturnBadRequest() throws Exception{
+    void givenAdminToken_andInvalidCredentials_whenRegister_thenReturnBadRequest() throws Exception{
+        String adminToken = getToken("admin", "Password123!!");
         UserRegistrationDTO dto = new UserRegistrationDTO(
                 "User",
                 "FelFormat",
@@ -119,12 +120,34 @@ class AuthControllerTest {
                 true
         );
 
-        String jsonBody = new ObjectMapper().writeValueAsString(dto);
+        String jsonBody = mapper.writeValueAsString(dto);
 
         mockMvc.perform(post("/users/register")
+                        .header("Authorization", "Bearer " + adminToken)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(jsonBody))
                 .andExpect(status().isBadRequest());
+
+    }
+
+    @Test
+    void givenUserToken_whenRegister_thenReturnForbidden() throws Exception {
+        String userToken = getToken("user", "Password123!!");
+
+        UserRegistrationDTO dto = new UserRegistrationDTO(
+                "forbiddenUser",
+                "Password123!!",
+                "USER",
+                true
+        );
+
+        String jsonBody = mapper.writeValueAsString(dto);
+
+        mockMvc.perform(post("/users/register")
+                .header("Authorization", "Bearer " + userToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonBody))
+                .andExpect(status().isForbidden());
 
     }
 
